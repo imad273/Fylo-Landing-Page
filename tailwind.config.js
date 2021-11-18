c7e56d4f0efd2c6f095a236fcf532e438f45608a
imad <abbadimad0123@gmail.com>
2021-11-18 10:21:12 +0100

    design clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         firstMain: 'hsl(217, 28%, 15%)',
         secondMain: 'hsl(218, 28%, 13%)',
         darkMain: 'hsl(216, 53%, 9%)',
+        testMos: 'hsl(219, 30%, 18%)',
         Cyan: 'hsl(176, 68%, 64%)',
         Blue: 'hsl(198, 60%, 50%)'
       },
